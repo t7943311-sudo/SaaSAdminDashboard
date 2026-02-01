ef32d72e0a7ea92e592b3af547a71d533d90473b
Tech <t7943311@gmail.com>
2026-02-01 04:05:58 +0000

    Try fixing this error: `Runtime ReferenceError: DropdownMenuSeparator is

--- src/app/admin/workspaces/page.tsx
+++ src/app/admin/workspaces/page.tsx
@@ -24,6 +24,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCollection, useFirestore, useMemoFirebase } from "@/firebase";
